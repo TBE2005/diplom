43a2af52f7619f18314739dbdd1f59ea6213585b
Your Name <20ins05@gmail.com>
2025-06-02 19:36:11 +0400

    first commit

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -13,15 +13,19 @@ const clientSecret = "7F7A47CFF9FECBDC770AECC5D97B0E7C05A598302978E21A252D2C3A57
 
 export async function GET(request: NextRequest) {
     const code = request.nextUrl.searchParams.get("code");
-    const responseToken = await fetch(`https://yoomoney.ru/oauth/token?grant_type=authorization_code&code=${code}&client_id=${clientId}&client_secret=${clientSecret}`)
-    const data = await responseToken.json();
-    const user = await fetchQuery(api.user.getByAccount, { account: data.account });
-    const response = NextResponse.redirect(new URL("/dashboard", request.url))
-    if (!user) {
-        const userId = await fetchMutation(api.user.create, { account: data.account, balance: data.balance });
-        response.cookies.set("user_id", userId, { httpOnly: true })
-    } else {
-        response.cookies.set("user_id", user._id, { httpOnly: true })
+    try {
+        const responseToken = await fetch(`https://yoomoney.ru/oauth/token?grant_type=authorization_code&code=${code}&client_id=${clientId}&client_secret=${clientSecret}`)
+        const data = await responseToken.json();
+        const user = await fetchQuery(api.user.getByAccount, { account: data.account });
+        const response = NextResponse.redirect(new URL("/dashboard", request.url))
+        if (!user) {
+            const userId = await fetchMutation(api.user.create, { account: data.account, balance: data.balance });
+            response.cookies.set("user_id", userId, { httpOnly: true })
+        } else {
+            response.cookies.set("user_id", user._id, { httpOnly: true })
+        }
+        return response;
+    } catch (error) {
+        console.log(error);
     }
-    return response;
 }
