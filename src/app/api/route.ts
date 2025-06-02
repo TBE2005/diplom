2452c223b551877770fa84216316aa7936517b8d
Your Name <20ins05@gmail.com>
2025-06-02 17:46:47 +0400

    first commit

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -6,6 +6,6 @@ export async function POST(request: NextRequest) {
     return NextResponse.json({ message: "Hello, world!" });
 }
 
-export async function GET(request: NextRequest) {
+export async function GET() {
     return NextResponse.json({ message: "Hello, world!" });
 }
