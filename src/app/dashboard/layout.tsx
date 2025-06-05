8f9e1e865c32d40a4004d6e76921cec4252700b3
Your Name <20ins05@gmail.com>
2025-06-05 20:10:03 +0400

    first commit

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,13 +1,19 @@
 import DashboardContent from "@/shared/dashboard";
+import { Center, Loader } from "@mantine/core";
+import { Suspense } from "react";
 export default async function DashboardLayout({
     children,
 }: {
     children: React.ReactNode,
 }) {
     return (
-        <DashboardContent>
-            {children}
-        </DashboardContent>
+        <Suspense fallback={<Center h="100vh" w="100vw" bg="gray.1">
+            <Loader />
+        </Center>}>
+            <DashboardContent>
+                {children}
+            </DashboardContent>
+        </Suspense>
     );
 
 }
