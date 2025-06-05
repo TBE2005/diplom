16764125c8109cc1f07877fecab5cd46006dc6bd
Your Name <20ins05@gmail.com>
2025-06-05 11:27:22 +0400

    first commit

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -8,10 +8,8 @@ import Link from 'next/link';
 import { FaUsers, FaMoneyBillWave, FaBullseye, FaBell, FaMoneyBill, FaCheck } from 'react-icons/fa';
 import { MdOutlineRequestPage } from "react-icons/md";
 import { useQuery } from 'convex/react';
-import { useEffect } from 'react';
+import { useEffect, Suspense } from 'react';
 import { api } from '../../../convex/_generated/api';
-import { Suspense } from 'react'
-
 
 const links = [
     { label: 'Мои цели', href: '/dashboard' },
@@ -29,7 +27,7 @@ const statData = [
     { icon: FaMoneyBill, label: 'Выплачено', value: '1,254', color: 'blue' },
 ];
 
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+function DashboardContent({ children }: { children: React.ReactNode }) {
     const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
     const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true);
     const searchParams = useSearchParams();
@@ -41,55 +39,64 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
         }
     }, [user]);
     const pathname = usePathname();
-    return <AppShell
-        header={{ height: 70 }}
-        navbar={{
-            width: 300,
-            breakpoint: 'sm',
-            collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
-        }}
-        padding="md"
-    >
-        <AppShell.Header>
-            <Group h="100%" px="md" pt="xs" pb="xs">
-                <Group>
-                    <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
-                    <Burger opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="sm" />
-                </Group>
+    
+    return (
+        <AppShell
+            header={{ height: 70 }}
+            navbar={{
+                width: 300,
+                breakpoint: 'sm',
+                collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
+            }}
+            padding="md"
+        >
+            <AppShell.Header>
+                <Group h="100%" px="md" pt="xs" pb="xs">
+                    <Group>
+                        <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
+                        <Burger opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="sm" />
+                    </Group>
 
-                <Group >
-                    {statData.map((stat, index) => (
-                        <Card key={index} withBorder w={100} h={50} p={5} m={0}>
-                            <Group p={0} m={0} gap={2}>
-                                <stat.icon style={{ color: `var(--mantine-color-${stat.color}-6)` }} />
-                                <Text c={stat.color}>{stat.value}</Text>
-                            </Group>
-                            <Text size="xs" c="dimmed">{stat.label}</Text>
-                        </Card>
-                    ))}
+                    <Group >
+                        {statData.map((stat, index) => (
+                            <Card key={index} withBorder w={100} h={50} p={5} m={0}>
+                                <Group p={0} m={0} gap={2}>
+                                    <stat.icon style={{ color: `var(--mantine-color-${stat.color}-6)` }} />
+                                    <Text c={stat.color}>{stat.value}</Text>
+                                </Group>
+                                <Text size="xs" c="dimmed">{stat.label}</Text>
+                            </Card>
+                        ))}
+                    </Group>
+                    <Group ml="auto">
+                        <CopyButton value={`https://diplom-liard-three.vercel.app/${user?.account}`} timeout={2000}>
+                            {({ copied, copy }) => (
+                                <Tooltip label={copied ? 'Ссылка скопирована' : 'Ссылка на страницу доната'} withArrow position="right">
+                                    <ActionIcon size={"xl"} color={copied ? 'teal' : ''} variant="light" onClick={copy}>
+                                        {copied ? <FaCheck /> : <MdOutlineRequestPage size={30} />}
+                                    </ActionIcon>
+                                </Tooltip>
+                            )}
+                        </CopyButton>
+                    </Group>
                 </Group>
-                <Group ml="auto">
-                    <CopyButton value={`https://diplom-liard-three.vercel.app/${user?.account}`} timeout={2000}>
-                        {({ copied, copy }) => (
-                            <Tooltip label={copied ? 'Ссылка скопирована' : 'Ссылка на страницу доната'} withArrow position="right">
-                                <ActionIcon size={"xl"} color={copied ? 'teal' : ''} variant="light" onClick={copy}>
-                                    {copied ? <FaCheck /> : <MdOutlineRequestPage size={30} />}
-                                </ActionIcon>
-                            </Tooltip>
-                        )}
-                    </CopyButton>
-                </Group>
-            </Group>
-        </AppShell.Header>
-        <AppShell.Navbar p="md">
-            {links.map((link) => (
-                <NavLink component={Link} key={link.href} {...link} active={pathname === link.href} />
-            ))}
-        </AppShell.Navbar>
-        <AppShell.Main>
-            <Suspense fallback={<div>Loading...</div>}>
+            </AppShell.Header>
+            <AppShell.Navbar p="md">
+                {links.map((link) => (
+                    <NavLink component={Link} key={link.href} {...link} active={pathname === link.href} />
+                ))}
+            </AppShell.Navbar>
+            <AppShell.Main>{children}</AppShell.Main>
+        </AppShell>
+    );
+}
+
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <DashboardContent>
                 {children}
-            </Suspense>
-        </AppShell.Main>
-    </AppShell >;
+            </DashboardContent>
+        </Suspense>
+    );
 }
