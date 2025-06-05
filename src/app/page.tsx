917221ca08192b62046ad432f0962e7d26b62697
Your Name <20ins05@gmail.com>
2025-06-05 15:41:00 +0400

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@mantine/core";
 import Link from "next/link";
 
 const clientId = "E69B1725E46F1E7855155A622D7952CF616D37C90D134955B4604150B175DF69"
-const redirectUri = "https://scrupulous-ladybug-152.convex.site/callback"
+const redirectUri = "https://sleek-barracuda-414.convex.site/callback"
 const scope = "account-info+operation-history+payment-p2p"
 const responseType = "code"
 // TODO: add scope
