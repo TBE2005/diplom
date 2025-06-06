4a5d24a08f57e12b42e77112a4ab4ce73c0421a4
Your Name <20ins05@gmail.com>
2025-06-06 08:12:09 +0400

    first commit

--- src/components/goal-template.tsx
+++ src/components/goal-template.tsx
@@ -1,10 +1,10 @@
-import { Flex, Text, Stack, Paper } from "@mantine/core";
+import { Flex, Text, Stack, Paper, Card } from "@mantine/core";
 import { Doc } from "../../convex/_generated/dataModel";
 
-export function GoalTemplate(settings: Doc<"goals"> & { collected: number, total: number, name: string}) {
+export function GoalTemplate(settings: Doc<"goals"> & { collected: number, total: number, name: string }) {
     const progress = settings.collected / settings.total;
     return (
-        <Paper bg={settings.backgroundColor} h={'100%'} w={'100%'} pos="relative" p={0} m={0}>
+        <Card bg={settings.backgroundColor}>
             <Paper w={`${progress * 100}%`} h={'100%'} c={settings.indicatorColor} pos="absolute" top={0} left={0} p={0} m={0} />
             <Stack gap="xs" pos="absolute" top={0} left={0} right={0} bottom={0} justify="center" align="center">
                 <Text c={settings.textColor}>{settings.name}</Text>
@@ -14,7 +14,7 @@ export function GoalTemplate(settings: Doc<"goals"> & { collected: number, total
                     <Text c={settings.textColor}>{settings.total}</Text>
                 </Flex>
             </Stack>
-        </Paper>
+        </Card>
     )
 
 }
