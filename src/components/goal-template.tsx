5b6d9ab553f8d06b84b4f5726cc7afbb1e653aef
Your Name <20ins05@gmail.com>
2025-06-06 08:19:33 +0400

    first commit

--- src/components/goal-template.tsx
+++ src/components/goal-template.tsx
@@ -3,8 +3,8 @@ import { Doc } from "../../convex/_generated/dataModel";
 
 export function GoalTemplate(settings: Doc<"goals"> & { collected: number, total: number, name: string }) {
     return (
-        <Card bg={settings.backgroundColor} h={150} pos="relative">
-            <Paper w={`${settings.collected / settings.total * 100}%`} h={150} color={settings.indicatorColor} pos="absolute" top={0} left={0} />
+        <Card bg={settings.backgroundColor} h={100} pos="relative">
+            <Paper w={`${settings.collected / settings.total * 100}%`} h={100} c={settings.indicatorColor} pos="absolute" top={0} left={0} />
             <Stack gap="xs" pos="absolute" top={0} left={0} right={0} bottom={0} justify="center" align="center">
                 <Text c={settings.textColor}>{settings.name}</Text>
                 <Flex align="center" gap="xs">
